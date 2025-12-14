d98b0a1c933728305f13553957697d5c9f5a633a
lltopk <liuyanoutsee@outlook.com>
2025-12-14 23:06:59 +0800

    update webpack

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,12 @@ module.exports = defineConfig({
       config.devtool = 'source-map'
     }
 
-    // 为 source-map 中的 source 路径加上 package 名称前缀，例如： webpack:///my-vue2/src/... 
+    //source-map 中的 source 路径是devtoolModuleFilenameTemplate
+    //可以在代码中加入debugger, 然后去浏览器查看映射文件的全路径
+    //格式为'webpack://[namespace][resource-path]?[loaders]')
+    //默认格式带有?哈希后缀, 每次映射的文件都带有随机的哈希后缀, 随机意味着我们无法一劳永逸的配置断点
+    //我们可以重写它，去掉哈希后缀, 同时用 package 名称作为namespace，方便在浏览器 devtools 中区分不同项目的 source 文件
+    // 例如： webpack:///my-vue2/src/... 
     config.output.devtoolModuleFilenameTemplate = `webpack://${appName}/[resource-path]`
     config.output.devtoolFallbackModuleFilenameTemplate = `webpack://${appName}/[resource-path]?[hash]`
   }
